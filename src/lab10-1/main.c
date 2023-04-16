959a985d9d83e6a7fc9463aa6859c0bcf66d265c
rogeryoungh <rogeryoungh@foxmail.com>
2023-04-16 13:52:22 +0800

    update: lab10-1

---
+++ src/lab10-1/main.c
@@ -0,0 +1,109 @@
+#include "../utils/gpio.h"
+#include "../utils/uart.h"
+
+extern void key_isr(void);
+extern void uart_send_byte(unsigned char byte);
+extern unsigned char uart_recv_byte();
+extern void uart_send_string(char *str);
+void delay(volatile unsigned int t) {
+  volatile unsigned int t2 = 0xFFFF;
+  while (t--)
+    for (; t2; t2--)
+      ;
+}
+
+void buzzer_init(void) {
+  GPD0CON |= 1 << 8;
+}
+
+void buzzer_on(void) {
+  GPD0DAT |= 1 << 2;
+}
+
+void buzzer_off(void) {
+  GPD0DAT &= ~(1 << 2);
+}
+
+void act1(int num) {
+  while (num--) {
+    GPJ2DAT ^= 1 << 0;
+    delay(100);
+    GPJ2DAT ^= 1 << 1;
+    delay(100);
+    GPJ2DAT ^= 1 << 2;
+    delay(100);
+    GPJ2DAT ^= 1 << 3;
+    delay(100);
+  }
+}
+
+void act2(int num) {
+  while (num--) {
+    GPJ2DAT ^= 1 << 3;
+    delay(100);
+    GPJ2DAT ^= 1 << 2;
+    delay(100);
+    GPJ2DAT ^= 1 << 1;
+    delay(100);
+    GPJ2DAT ^= 1 << 0;
+    delay(100);
+  }
+}
+
+void key_handle() {
+  volatile unsigned char key_code = EXT_INT_0_PEND & 0xC;
+  VIC0ADDRESS = 0;
+
+  EXT_INT_0_PEND |= 0xC;
+
+  if (key_code == 0x04) { /* SW4 */
+    uart_send_string("\r\nSW4 PRESSED\r\n");
+    act1(10);
+  } else if (key_code == 0x08) { /* SW5 */
+    uart_send_string("\r\nSW5 PRESSED\r\n");
+    act2(10);
+  }
+
+  /* toggle LED1 */
+  else if (key_code == 0x08) {
+    int num = 10;
+  }
+  /* toggle LED2 */
+}
+
+int main() {
+
+  GPJ2CON &= ~(0xFF << 0);
+  GPJ2CON |= ((0x01 << 0) | (0x01 << 4) | (0x01 << 8) |
+              (0x01 << 12)); //((0x01 << 0) | (0x01 << 4));
+  GPJ2DAT |= (0xFF << 0);
+  GPH0CON |= 0xFF << 8;
+  EXT_INT_0_CON &= ~(0xFF << 8);
+  EXT_INT_0_CON |= (2 << 8) | (2 << 12);
+  EXT_INT_0_MASK &= ~0xC;
+  uart_send_string("\r\nUART Test in S5PV210\r\n");
+
+  VIC0INTSELECT &= ~0xC;
+  VIC0INTENABLE |= 0xC;
+  VIC0VECTADDR2 = (int)key_isr;
+  VIC0VECTADDR3 = (int)key_isr;
+
+
+#define STR1 "SW4+5T"
+#define STR2 "SW4+10T"
+  int p1 = 0, p2 = 0;
+  while (1) {
+    char byte = uart_recv_byte();
+    p1 = (byte == STR1[p1]) ? p1 + 1 : 0;
+    p2 = (byte == STR2[p2]) ? p2 + 1 : 0;
+    if (p1 == sizeof(STR1)) {
+      act1(5);
+      p1 = 0;
+    } else if (p2 == sizeof(STR2)) {
+      act2(10);
+      p2 = 0;
+    }
+  }
+
+  return 0;
+}
